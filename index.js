851096247eefecb3b7e89777f7520e3440179800
Richard Cargill <richard.r.cargill@gmail.com>
2019-04-27 17:57:34 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const GoogleSpreadsheet = require('google-spreadsheet');
+const {send} = require('micro');
+const sanitizeHtml = require('sanitize-html');
+const doc = new GoogleSpreadsheet('1FTrFBP-ss-R76XncyJv9znOJCAEMgrWqRfqAVaS5HNs');
+
+const getRows = () => new Promise(function(resolve, reject) {
+  doc.getInfo(function(err, info) {
+    if(err) reject(err);
+
+    resolve(info.worksheets[0]);
+  });
+}).then(function(sheet) {
+  return new Promise(function(resolve, reject) {
+    sheet.getRows({
+      offset: 1,
+      orderby: 'col2'
+    }, function (err, rows) {
+      if(err) reject(err);
+      resolve(rows);
+    });
+  });
+});
+
+function pluckContent(accu, current, index) {
+  const {title, tags, content} = current;
+  if(!title) return accu;
+  return [...accu, {
+    title: sanitizeHtml(title),
+    tags: sanitizeHtml(tags),
+    children: sanitizeHtml(content)}];
+}
+
+async function handler(req, res) {
+  const rows = await getRows();
+
+  const formattedResponse = rows.reduce(pluckContent, []);
+
+  send(res, 200, formattedResponse);
+}
+
+module.exports = handler;
