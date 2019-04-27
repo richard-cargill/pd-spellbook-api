62fd96f05aef50912bd89ee6c5cac475278412a1
Richard Cargill <richard.r.cargill@gmail.com>
2019-04-27 18:16:40 +0100

    updating cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const GoogleSpreadsheet = require('google-spreadsheet');
 const {send} = require('micro');
 const sanitizeHtml = require('sanitize-html');
+const cors = require('micro-cors')();
 const doc = new GoogleSpreadsheet('1FTrFBP-ss-R76XncyJv9znOJCAEMgrWqRfqAVaS5HNs');
 
 const getRows = () => new Promise(function(resolve, reject) {
@@ -38,4 +39,4 @@ async function handler(req, res) {
   send(res, 200, formattedResponse);
 }
 
-module.exports = handler;
+module.exports = cors(handler);
